fda0cab91d8e773c1b48ec1ef6f116896fc469b5
2XI <shengxi_hu@yeah.net>
2017-03-17 23:10:05 +0800

    add todos

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,75 @@
-//index.js
-//获取应用实例
-var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+      items: [
+        {
+            txt: 'Add something!',
+            ctype: 'circle',
+            completed: false,
+            id: 0
+            }
+      ]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
+  init: function(){
+      this.setData({
+          all: '',
+          completed: ''
       })
-    })
+  },
+  onLoad: function (){
+      this.init()
+  },
+  setComplete: function(e){
+      this.init()
+      var _id = e.target.id
+      var data = this.data.items
+      data.filter(function(element, index){
+         if(element.id == _id) {
+            _id = index
+         }
+      })
+      data[_id].completed = !data[_id].completed
+      this.setData({
+          items: data
+      })
+  },
+  deleteItem: function(e){
+      this.init()
+      var _id = e.target.id
+      var data = this.data.items
+      data.filter(function(element, index){
+         if(element.id == _id) {
+            _id = index
+         }
+      })
+      data.splice(_id, 1)
+      this.setData({
+          items: data
+      })
+  },
+  bindAll: function() {
+      this.setData({
+          all: 'act',
+          completed: ''
+      })
+  },
+  bindCompleted: function(){
+        this.setData({
+            all: '',
+            completed: 'act',
+        })
+  },
+  finish: function(e){
+      var val = e.detail.value
+      var data = this.data.items
+      data.push({
+        txt: val,
+        ctype: 'circle',
+        completed: false,
+        id: data.length + 1
+      })
+      this.setData({
+            items: data,
+            val: ''
+      })
   }
 })
